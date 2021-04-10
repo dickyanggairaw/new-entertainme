ea1088b18fe63a67fb0575652bfd7a13db53a8c1
dickyanggairaw <dickyanggairaw@gmail.com>
2021-04-10 12:19:10 +0700

    fix typo folder

--- server/orchestrator/app.js
+++ server/orchestrator/app.js
@@ -3,7 +3,7 @@ const { ApolloServer } = require('apollo-server')
 const server = new ApolloServer({ 
   modules: [
     require('./module/movies'),
-    require('./module/category')
+    require('./module/series')
   ]
  });
 
